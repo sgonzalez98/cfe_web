2b6da352129174c942672ba6d575f275eed84f7a
Sergio Gonzalez <sgonzalez.cna@gmail.com>
2023-09-21 17:53:36 -0500

    Se quita referencia

--- src/ui/web/Testimoni.tsx
+++ src/ui/web/Testimoni.tsx
@@ -7,87 +7,89 @@ import Stars from "../../../public/assets/icons/stars.svg";
 import ArrowBack from "../../../public/assets/icons/eva_arrow-back-fill.svg";
 import ArrowNext from "../../../public/assets/icons/eva_arrow-next-fill.svg";
 
-const Testimoni = ({
-  listTestimoni = [
+
+const listTestimoni = [
+  {
+    name: "iezh Robert",
+    image: "/assets/people-3.png",
+    city: "Warsaw",
+    country: "Poland",
+    rating: "4.5",
+    testimoni:
+      "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+  },
+  {
+    name: "iezh Robert",
+    image: "/assets/people-3.png",
+    city: "Warsaw",
+    country: "Poland",
+    rating: "4.5",
+    testimoni:
+      "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+  },
+  {
+    name: "iezh Robert",
+    image: "/assets/people-3.png",
+    city: "Warsaw",
+    country: "Poland",
+    rating: "4.5",
+    testimoni:
+      "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+  },
+  {
+    name: "iezh Robert",
+    image: "/assets/people-3.png",
+    city: "Warsaw",
+    country: "Poland",
+    rating: "4.5",
+    testimoni:
+      "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+  },
+];
+
+const settings = {
+  dots: true,
+  customPaging: function () {
+    return (
+      <a>
+        <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
+      </a>
+    );
+  },
+  dotsClass: "slick-dots w-max absolute mt-20",
+  infinite: true,
+  speed: 500,
+  slidesToShow: 3,
+  slidesToScroll: 2,
+  responsive: [
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+      breakpoint: 770,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+      },
     },
     {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
-    {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
-    },
-    {
-      name: "iezh Robert",
-      image: "/assets/people-3.png",
-      city: "Warsaw",
-      country: "Poland",
-      rating: "4.5",
-      testimoni:
-        "Wow... I am very happy to use this VPN, it turned out to be more than my expectations and so far there have been no problems. LaslesVPN always the best",
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
     },
   ],
-}) => {
-  const settings = {
-    dots: true,
-    customPaging: function () {
-      return (
-        <a className="">
-          <span className="mx-2 rounded-l-full rounded-r-full h-4 w-4 block cursor-pointer transition-all "></span>
-        </a>
-      );
-    },
-    dotsClass: "slick-dots w-max absolute mt-20  ",
-    infinite: true,
-    speed: 500,
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    responsive: [
-      {
-        breakpoint: 770,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+};
+
+const Testimoni = () => {
   const [sliderRef, setSliderRef] = useState<null|Slider>(null);
 
   return (
     <>
       <Slider
-        {...settings}
         arrows={false}
-        ref={setSliderRef}
+        // ref={setSliderRef}
         className="flex items-stretch justify-items-stretch"
+        {...settings}
       >
         {listTestimoni.map((listTestimonis, index) => (
           <div className="px-3 flex items-stretch" key={index}>
@@ -125,13 +127,13 @@ const Testimoni = ({
         <div className="flex flex-none justify-between w-auto mt-14">
           <div
             className="mx-4 flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickPrev}
+            onClick={sliderRef ? sliderRef.slickPrev : () => {}}
           >
             <Image className="h-6 w-6 " src={ArrowBack} alt="Flecha Atras" />
           </div>
           <div
             className="flex items-center justify-center h-14 w-14 rounded-full bg-white border-orange-500 border hover:bg-orange-500 hover:text-white-500 transition-all text-orange-500 cursor-pointer"
-            onClick={sliderRef?.slickNext}
+            onClick={sliderRef ? sliderRef.slickNext : () => {}}
           >
             <Image className="h-6 w-6 " src={ArrowNext} alt="Flecha Adelante" />
           </div>
